f2a659d259b0276aa8ef30de5d1b8893555bc280
matchild <mt.child@gmx.com>
2023-08-06 14:52:41 +0200

    Put functions in utils.py

---
+++ utils.py
@@ -0,0 +1,135 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+from stochastic.processes import FractionalBrownianMotion
+from torch.utils.data import TensorDataset, DataLoader, Dataset
+
+
+
+
+def generate_dataset(N_SIMS:int, trajectory_length:int):
+  '''
+  This function generates a dataset of fractional brownian motion trajectories.
+
+  N_SIMS: number of simulations to be generated
+  trajectory_length: number of steps in each trajectory
+  '''
+
+
+  X=np.zeros((N_SIMS,trajectory_length ,1)) #Create empty X vector 
+  Y=np.zeros(N_SIMS) #Create empty y vector
+
+
+
+  for i in range(N_SIMS):
+    h=round(np.random.uniform(0.01,0.90), 4) #Choose Hurst paramenter from uniform distribution (only take the first 4 decimal digits)
+    f = FractionalBrownianMotion(hurst=h, t=1)
+    X[i]= f.sample(trajectory_length-1).reshape(trajectory_length, 1)
+    Y[i]=h
+
+    if i%10000 == 0:
+      print('Simulations Generated:',i)
+
+  Y=Y.reshape(-1,1)
+
+  #Split train and test datasets (70/30)
+  X_train=X[:int(N_SIMS*0.7)]
+  Y_train=Y[:int(N_SIMS*0.7)]
+
+
+  X_test=X[int(N_SIMS*0.7):]
+  Y_test=Y[int(N_SIMS*0.7):]
+
+  return X_train, Y_train, X_test, Y_test
+
+
+def DatasetMAE(loader, model, device):
+  loss=0
+  num_samples = 0
+  model.eval()  # set model to evaluation mode
+  with torch.no_grad():
+      for x, y in loader:
+          x = x.to(device=device) # move to device, e.g. GPU
+          y = y.to(device=device)
+          scores = model(x)
+
+          loss+=(torch.abs(scores-y)).sum()
+          num_samples += scores.size(0)
+
+      print('Got an overall MAE of ', loss.item()/num_samples)
+  return loss.item()/num_samples    #Return average loss
+
+
+class SimulationDatset(Dataset):
+
+  def __init__(self, X, Y):
+
+    #scaling features
+    X=(X - np.mean(X, axis=1, keepdims=True)) / np.std(X, axis=1, keepdims=True) #Normalize X
+    Y=Y #No need to normalize y
+
+    #converting to torch tensors
+    self.X=torch.tensor(X, dtype=torch.float32)
+    self.Y=torch.tensor(Y, dtype=torch.float32)
+
+  def __len__(self):
+      return len(self.Y)
+
+  def __getitem__(self, idx):
+      return self.X[idx], self.Y[idx]
+      
+
+
+
+class DeepBrownianEncoder(torch.nn.Module):
+
+    def __init__(self, embedding_dim=1, num_layers=1, num_heads=1, trajectory_length=102):
+        super(DeepBrownianEncoder, self).__init__()
+
+        self.embedding_dim=embedding_dim
+        self.num_layers=num_layers
+        self.num_heads=num_heads
+        self.trajectory_length=trajectory_length
+        
+        self.linear1 = torch.nn.Linear(1, embedding_dim)
+        self.encoder_layer = torch.nn.TransformerEncoderLayer(d_model=self.embedding_dim, nhead=num_heads, batch_first=True)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=self.num_layers)
+        self.linear2 = torch.nn.Linear(self.trajectory_length*self.embedding_dim, 1)
+
+
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+        
+        x = self.linear1(x)
+        x = self.transformer_encoder(x)
+        x = x.view(batch_size, -1)
+        x = self.linear2(x)
+        return x
+    
+
+
+def train(dataloader, model, loss_fn, optimizer, device):
+    losses=[]
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        losses.append(loss.item())
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        
+    return losses
